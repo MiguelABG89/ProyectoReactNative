0f077719dd907edaaae0d462a6a2aa5778d22f99
Miguel <miguel891906@gmail.com>
2024-01-26 12:28:57 +0100

    inicial commit

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// App.js
+
+import { Amplify } from 'aws-amplify';
+import amplifyconfig from './src/amplifyconfiguration.json';
+Amplify.configure(amplifyconfig);
 
 export default function App() {
   return (
