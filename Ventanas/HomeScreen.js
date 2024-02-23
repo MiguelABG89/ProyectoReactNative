0a7c41b58b6474c9055d998598365fd41bf75c70
Miguel <miguel891906@gmail.com>
2024-02-23 12:52:48 +0100

    commit with api modifications

--- Ventanas/HomeScreen.js
+++ Ventanas/HomeScreen.js
@@ -1,21 +1,121 @@
+// import { useState } from 'react';
+// import { Text, TextInput, View, Button, Alert, Image, navigation } from 'react-native';
+// import ModalDropdown from 'react-native-modal-dropdown';
+// import styles from '../estilos/estilos'
+// import {signOut} from 'aws-amplify/auth'
+// import { get } from 'aws-amplify/api';
+
+// function HomeScreen({ navigation }) {
+//     const [selectedLanguage, setSelectedLanguage] = useState("Seleccione un idioma");
+//     const languages = [
+//         'Español',
+//         'English',
+//         'Français',
+//         'deutsch',
+//         '中国人'
+//     ];
+
+//     const [user, setUser] = useState("");
+//     const [code, setCode] = useState("");
+//     const [apiResponse, setApiResponse] = useState(""); // Nuevo estado para almacenar la respuesta de la API
+
+//     const handleLanguageSelect = (index, value) => {
+//         setSelectedLanguage(value);
+
+//         // Navegar al componente correspondiente al idioma seleccionado
+//         switch (value) {
+//             case 'Español':
+//                 navigation.navigate('Register');
+//                 break;
+//             case 'English':
+//                 navigation.navigate('englishRegister');
+//                 break;
+//             case 'Français':
+//                 navigation.navigate('frenchRegister');
+//                 break;
+//             default:
+//                 navigation.navigate('Register'); // Por defecto, regresa a Español
+//         }
+//     };
+
+//     async function getTodo() {
+//         try {
+//             const restOperation = await get({ 
+//                 apiName: 'ProyectoReactNative',
+//                 path: '/hello'
+//             });
+    
+//             const { body } = await restOperation.response;
+//             const responseData = await body.json();
+            
+//             // Convierte el objeto a cadena de texto
+//             const responseString = JSON.stringify(responseData);
+    
+//             setApiResponse(responseString); // Almacena la respuesta en el estado
+//             console.log('GET call succeeded: ', responseData);
+//         } catch (error) {
+//             console.log('GET call failed: ', error);
+//         }
+//     }
+
+//     async function handleSignOut() {
+//         try {
+//           await signOut();
+//           navigation.navigate("Inicio");
+//         } catch (error) {
+//           console.log('error signing out: ', error);
+//         }
+//     }
+
+//     return (
+//         <View style={styles.estructure}>
+
+//             <ModalDropdown
+//                 options={languages}
+//                 defaultValue={selectedLanguage}
+//                 onSelect={handleLanguageSelect}
+//             />
+
+//             <Image
+//                 source={require('../assets/Logo-FDP.jpg')}
+//                 style={styles.image}
+//             />
+
+//             <Button
+//                 color={styles.buttons.color}
+//                 title="Llamar api"
+//                 onPress={getTodo} 
+//             />
+
+//             {/* Muestra la respuesta de la API en el componente Text */}
+//             <Text>{apiResponse}</Text>
+
+//             <Button
+//                 color={styles.buttons.color}
+//                 title="Cerrar sesión"
+//                 onPress={handleSignOut} 
+//             />
+            
+//         </View>
+//     );
+// }
+
+// export default HomeScreen;
 import { useState } from 'react';
 import { Text, TextInput, View, Button, Alert, Image, navigation } from 'react-native';
 import ModalDropdown from 'react-native-modal-dropdown';
-import styles from '../estilos/estilos'
-import {signOut} from 'aws-amplify/auth'
+import styles from '../estilos/estilos';
+import { signOut } from 'aws-amplify/auth';
 import { get } from 'aws-amplify/api';
 
 function HomeScreen({ navigation }) {
     const [selectedLanguage, setSelectedLanguage] = useState("Seleccione un idioma");
-    const languages = [
-        'Español',
-        'English',
-        'Français',
-        'deutsch',
-        '中国人'];
+    const languages = ['Español', 'English', 'Français', 'deutsch', '中国人'];
 
-    const [user, setUser] = useState("")
-    const [code, setCode] = useState("")
+    const [user, setUser] = useState("");
+    const [code, setCode] = useState("");
+    const [apiResponse, setApiResponse] = useState("");
+    const [inputValue, setInputValue] = useState(""); // Nuevo estado para almacenar el valor del input
 
     const handleLanguageSelect = (index, value) => {
         setSelectedLanguage(value);
@@ -29,52 +129,74 @@ function HomeScreen({ navigation }) {
                 navigation.navigate('englishRegister');
                 break;
             case 'Français':
-                navigation.navigate('frenchRegister')
+                navigation.navigate('frenchRegister');
+                break;
             default:
                 navigation.navigate('Register'); // Por defecto, regresa a Español
         }
     };
 
-   
-
     async function getTodo() {
         try {
-            const restOperation = get({ 
-            apiName: 'ProyectoReactNative',
-            path: '/items',
-            options: {
-                queryParams: {
-                  id: '1234'
-                }
-              } 
+            const restOperation = await get({
+                apiName: 'ProyectoReactNative',
+                path: '/hello'
             });
+
             const { body } = await restOperation.response;
-            const str = await body.json();
-            
-            console.log('GET call succeeded: ', str );
+            const responseData = await body.json();
+
+            const responseString = JSON.stringify(responseData);
+
+            setApiResponse(responseString);
+            console.log('GET call succeeded: ', responseData);
         } catch (error) {
             console.log('GET call failed: ', error);
         }
     }
 
+    async function getTodoWithParams() {
+        try {
+            const restOperation = await get({
+                apiName: 'ProyectoReactNative',
+                path: '/items',
+                // Pasar parámetros a la API
+                options:{
+
+                    queryParams: {
+                        param: inputValue
+                    }
+                }
+            });
+
+            const { body } = await restOperation.response;
+            const responseData = await body.json();
+
+            const responseString = JSON.stringify(responseData);
+
+            setApiResponse(responseString);
+            console.log('GET call with params succeeded: ', responseData);
+        } catch (error) {
+            console.log('GET call with params failed: ', error);
+        }
+    }
+
     async function handleSignOut() {
         try {
-          await signOut();
-          navigation.navigate("Inicio")
+            await signOut();
+            navigation.navigate("Inicio");
         } catch (error) {
-          console.log('error signing out: ', error);
+            console.log('error signing out: ', error);
         }
-      }
+    }
 
     return (
         <View style={styles.estructure}>
-
-        <ModalDropdown
-            // El valor inicial no es considerado una opcion de la lista y salta un mini error
-            options={languages}
-            defaultValue={selectedLanguage}
-            onSelect={handleLanguageSelect}
-        />
+            <ModalDropdown
+                options={languages}
+                defaultValue={selectedLanguage}
+                onSelect={handleLanguageSelect}
+            />
 
             <Image
                 source={require('../assets/Logo-FDP.jpg')}
@@ -83,17 +205,32 @@ function HomeScreen({ navigation }) {
 
             <Button
                 color={styles.buttons.color}
-                title="Llamar api"
-                onPress={getTodo} 
+                title="Llamar API"
+                onPress={getTodo}
             />
+
+            <TextInput
+                style={styles.input}
+                placeholder="Ingrese parámetro"
+                onChangeText={(text) => setInputValue(text)}
+                value={inputValue}
+            />
+
+            <Button
+                color={styles.buttons.color}
+                title="Llamar API con parámetros"
+                onPress={getTodoWithParams}
+            />
+
+            <Text>{apiResponse}</Text>
+
             <Button
                 color={styles.buttons.color}
                 title="Cerrar sesión"
-                onPress={handleSignOut} 
+                onPress={handleSignOut}
             />
-            
         </View>
     );
 }
 
-export default HomeScreen
+export default HomeScreen;
